cb86ca6571ed1ff3a3cb8eda0a1ae0b5613716b0
Uotan Heinz <61697058+Uotan@users.noreply.github.com>
2021-09-02 21:41:30 +0700

    1st commit

---
+++ WorldSkills/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WorldSkills.Pages;
+//using WorldSkills.Classes;
+
+namespace WorldSkills
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameClass._mainframe = WinFrame;
+            FrameClass._mainframe.Navigate(new LogIn());
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Ellipse_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void WinFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void WinFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WorldSkills/Pages/LogIn.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WorldSkills;
+
+namespace WorldSkills.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Page
+    {
+        TestEntities context;
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLog_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                context = new TestEntities();
+                Users log = context.Users.FirstOrDefault(x => x.name == tbLog.Text && x.password == tbPass.Text);
+                if (log!=null)
+                {
+                    if (log.type == 1)
+                    {
+                        MessageBox.Show("You're admin");
+                    }
+                    FrameClass._mainframe.Navigate(new PIC());
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //throw;
+            }
+        }
+
+        private void tbLog_MouseMove(object sender, MouseEventArgs e)
+        {
+            tbLog.Background = Brushes.DarkGray;
+        }
+
+        private void tbLog_MouseLeave(object sender, MouseEventArgs e)
+        {
+            tbLog.Background = Brushes.Gray;
+        }
+    }
+}
